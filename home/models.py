352bb606f1c0898b6392018ba61d8cc0995e3977
user <yegansmaili@gmail.com>
2025-06-04 21:21:07 +0330

    implement models and serializers for video course

--- home/models.py
+++ home/models.py
@@ -218,3 +218,74 @@ class ContactSubmission(models.Model):
 
     def __str__(self):
         return f'{self.email} : {self.subject}'
+
+
+# Course Models
+
+class Course(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, allow_unicode=True, )
+    description = RichTextField()
+    course_image = models.ImageField(upload_to='course/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Video(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        ('private', 'Private'),
+    ]
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, allow_unicode=True, )
+    description = RichTextField(blank=True)
+    video_file = models.FileField(upload_to='videos/')
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='videos')
+    duration = models.DurationField()
+    priority = models.PositiveIntegerField(unique=True, verbose_name='video order')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.title} - {self.course.title}'
+
+
+class CourseOrder(models.Model):
+    PAYMENT_STATUS = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    course = models.ForeignKey(Course, on_delete=models.PROTECT, related_name='orders')
+    payment_status = models.CharField(choices=PAYMENT_STATUS, max_length=10 , default='pending')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order #{self.id} - {self.user} - {self.course}'
